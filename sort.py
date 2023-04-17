7c4a84b02aead776c6b38a643862728db2f7c830
maryam <56215980+maryam-mirzaei@users.noreply.github.com>
2023-04-18 01:59:51 +0430

    add test data

---
+++ sort.py
@@ -0,0 +1,13 @@
+def sort(sortkey):
+	if sortkey=="price":
+		sort all files by prices
+		save in sorted.txt
+
+	elif sortkey=="star":
+		sort all files by stars
+		save in sorted.txt
+	else:
+		no sort
+
+
+sortkey can be : "price","stars","Default"
