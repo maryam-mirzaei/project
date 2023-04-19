30c16d05940dc729a3ebd1c0350d597ad546ba83
rezaasghariir <reza.asghari.ir@gmail.com>
2023-04-20 01:01:42 +0330

    changed style of page

--- page.py
+++ page.py
@@ -4,16 +4,23 @@ import dash_bootstrap_components as dbc # pip install dash-bootstrap-components
 import dash_html_components as html # pip install dash-html-components
 
 app = Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP])
-app.layout = html.Div([
-    'مرتب کردن بر اساس:',
-    dcc.Dropdown(   options=[
-       {'label': 'پیشفرض', 'value': 'Default'},
-       {'label': 'قیمت', 'value': 'Price'},
-       {'label': 'امتیاز', 'value': 'Rate'},
-   ],
-   value='Default', id='demo-dropdown'),
+app.layout = dbc.Container([
+    html.H1('جستجو در دیجی کالا', className='text-center'),
+    dbc.Row([
+        dbc.Col([
+            dbc.Label("مرتب کردن بر اساس:"),
+            dbc.Select(
+                    options=[
+                        {'label': 'پیشفرض', 'value': 'Default'},
+                        {'label': 'قیمت', 'value': 'Price'},
+                        {'label': 'امتیاز', 'value': 'Rate'},
+                    ],
+                    value='Default', id='demo-dropdown'
+                )])
+    ], className='my-4'),
     html.Div(id='dd-output-container')
-],style={"width": "30vh", "color": "black"})
+],style={"width": "50%", "color": "black"})
+
 
 
 @app.callback(
